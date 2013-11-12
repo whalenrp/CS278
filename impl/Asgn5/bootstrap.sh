ed8447cf57f516de9afbe36a1eb1675b8ee34522
seanstory <sean.j.story@vanderbilt.edu>
2013-11-12 17:57:05 -0600

    added install for mongoDB

--- impl/Asgn5/bootstrap.sh
+++ impl/Asgn5/bootstrap.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+sudo apt-get update
 
+#Install the newest version of MongoDM  
+  mkdir -p /data/db 
+  chown -R vagrant /data
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10 #install new version
+  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-10gen
 
 set -e
 

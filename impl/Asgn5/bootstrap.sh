d354ae5938d8c4e73e4637632de42669723a970c
seanstory <sean.j.story@vanderbilt.edu>
2013-11-13 16:59:18 -0600

    added install of nosetests and Jython

--- impl/Asgn5/bootstrap.sh
+++ impl/Asgn5/bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 sudo apt-get update
 
+sudo apt-get install -y python-pip
+sudo easy_install nose
+
 #Install the newest version of MongoDM  
   mkdir -p /data/db 
   chown -R vagrant /data
@@ -8,6 +11,15 @@ sudo apt-get update
   echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
   sudo apt-get update
   sudo apt-get install -y mongodb-10gen
+  sudo pip install pymongo
+
+# install jython
+  sudo apt-get install -y curl
+  curl http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-b1/jython-installer-2.7-b1.jar -O jython-installer-2.7-b1.jar
+  sudo java -jar jython-installer-2.7-b1.jar -s -d /usr/lib/jvm/jython
+  sudo echo "JYTHON_HOME=/usr/lib/jvm/jython" | sudo tee -a /etc/environment
+  export JYTHON_HOME=/user/lib/jvm/jython
+  source /etc/environment
 
 set -e
 
---
+++ impl/Asgn5/mongo-files/py-tests/tests.py
@@ -0,0 +1,4 @@
+"""
+This file is for the unit testing of the python component of the Mobile Receipts
+project.
+"""
